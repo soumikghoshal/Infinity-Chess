c3d3543580d5081937276ea469537d35bf7d0b4e
ChandraSekharGhosh <chandrasekharghosh44@gmail.com>
2025-10-12 16:55:05 +0530

    6 end

--- Events/global.js
+++ Events/global.js
@@ -1,6 +1,11 @@
 import { ROOT_DIV } from "../Helper/constants.js";
 import { globalState } from "../index.js";
 
+function whitePawnClick() {
+    console.log("White Pawn Clicked");
+    
+}
+
 function GlobalEvent() {
     ROOT_DIV.addEventListener("click", function (event) {
         if (event.target.localName === "img") {
@@ -8,6 +13,9 @@ function GlobalEvent() {
             const flatArray = globalState.flat();
             const square = flatArray.find((el) => el.id == clickId);
             //console.log(square);
+            if (square.piece.piece_name == "WHITE_PAWN") {
+                whitePawnClick();
+            }
         }
         
     });
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # This is Infinity Chess
+part 6 completed
